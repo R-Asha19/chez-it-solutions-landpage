4fb72b65729d396c4675bb79200499670b6a47ed
Hema <ashahema2005@gmail.com>
2026-07-09 22:50:43 +0530

    Fix missing Menu/X import in Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
-
+import { Menu, X } from "lucide-react";
 import { useLeadModal } from "../context/LeadModalContext.jsx";
 import logo from "../assets/logo.jpeg";
 
