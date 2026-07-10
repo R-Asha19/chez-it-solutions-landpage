69d1a28fe7c5b8c46c6041e02cbc7ae99d590c36
Hema <ashahema2005@gmail.com>
2026-07-10 13:18:13 +0530

    Force IPv4 DNS resolution app-wide to fix Render-to-Gmail connectivity

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import rateLimit from "express-rate-limit";
 import connectDB from "./config/db.js";
 import leadRoutes from "./routes/leadRoutes.js";
+
 
 dotenv.config();
 
